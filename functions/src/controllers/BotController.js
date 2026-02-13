205fdc610263077c66d5d771d98e1e913b6e4d61
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2026-02-12 22:43:03 -0300

    listo bot

--- functions/src/controllers/BotController.js
+++ functions/src/controllers/BotController.js
@@ -1,8 +1,8 @@
+import { EntityNotFoundError } from "../../errors/EntityNotFoundError.js";
 import { OutOfRangeError } from "../../errors/OutOfRangeError.js";
-import { BotService } from "../services/botService.js";
+import { BotService } from "../services/BotService.js";
 
 class BotController {
-  
   constructor(botSer = new BotService()) {
     this.botSer = botSer;
   }
@@ -22,6 +22,44 @@ class BotController {
         res.status(400).json({ error: error.message });
       } else {
         res.status(500).json({ error: "error del server" });
+      }
+    }
+  };
+
+  getBotById = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const boti = await this.botSer.getBotById(id);
+      res.status(200).json(boti);
+    } catch (error) {
+      if (error instanceof EntityNotFoundError) {
+        res.status(404).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: "error del server" });
+      }
+    }
+  };
+
+  updateBot = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, generation, processing, memory, modules } = req.body;
+      const nBot = await this.botSer.updateBot(id, {
+        name,
+        generation,
+        processing,
+        memory,
+        modules,
+      });
+
+      res.status(200).json(nBot);
+    } catch (error) {
+      if (error instanceof EntityNotFoundError) {
+        res.status(404).json({ error: error.message });
+      } else if (error instanceof OutOfRangeError) {
+        res.status(400).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: "Error del server" });
       }
     }
   };
