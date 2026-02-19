25e43ea097956535c5d44a09ee89de8062f8d33c
esteban cardozo <esteban.cardozo.wec@gmail.com>
2026-02-19 12:53:02 -0300

    repo review

---
+++ functions/src/repositories/AttemptsRepository.js
@@ -0,0 +1,37 @@
+import { DuplicateError } from "../../errors/DuplicateError.js";
+import { EntityNotFoundError } from "../../errors/EntityNotFoundError.js";
+import { readAndParse } from "../utils.js";
+import fs from "fs/promises"
+
+class AttemptsRepository {
+
+  url = "./functions/src/repositories/league.json";
+
+  async getAttemptsById(id) {
+
+    const data = await readAndParse(this.url);
+    const atte = await data.attempts.find(a => a.id === Number(id));
+    return atte;
+
+  }
+
+  async createAttempts(attempt) {
+
+    const data = await readAndParse(this.url);
+    const validate = await this.getAttemptsById(attempt.id);
+    if (validate) {
+      throw new DuplicateError("Ya existe attempt con ese id");
+    }
+    data.attempts.push(attempt);
+    await fs.writeFile(this.url, JSON.stringify(data, null, 2));
+    return attempt;
+
+
+
+  }
+
+
+
+}
+
+export { AttemptsRepository };
--- functions/src/repositories/BotRepository.js
+++ functions/src/repositories/BotRepository.js
@@ -1,6 +1,7 @@
 import fs from "fs/promises";
-import { readAndParse } from "../utils";
-import { OutOfRangeError } from "../../errors/OutOfRangeError";
+import { readAndParse } from "../utils.js";
+import { DuplicateError } from "../../errors/DuplicateError.js";
+import { EntityNotFoundError } from "../../errors/EntityNotFoundError.js";
 
 class BotRepository {
   url = "./functions/src/repositories/league.json";
@@ -16,28 +17,40 @@ class BotRepository {
 
   async createBot(bot) {
     const data = await readAndParse(this.url);
-    const validateId = data.bots.find((i) => i.id === Number(bot.id));
+    const validateId = await this.getBotById(bot.id);
     if (validateId) {
-      throw new OutOfRangeError("Ya existe un bot con ese id, ponle otro");
+      throw new DuplicateError("Ya existe un bot con ese id, ponle otro");
     }
     data.bots.push(bot);
-    return fs.writeFile(this.url, JSON.stringify(data, null, 2));
+    await fs.writeFile(this.url, JSON.stringify(data, null, 2));
+    return bot;
   }
 
   async updateBot(id, datos) {
     const data = await readAndParse(this.url);
     const botIndex = data.bots.findIndex((d) => d.id === id);
-    if (botIndex > -1) {
-      data.bots[botIndex] = { ...data.bots[botIndex], ...datos };
+    if (botIndex < 0) {
+      throw new EntityNotFoundError("No se encontró el bot a actualizar");
     }
-    return fs.writeFile(this.url, JSON.stringify(data, null, 2));
+    data.bots[botIndex] = { ...data.bots[botIndex], ...datos };
+
+    await fs.writeFile(this.url, JSON.stringify(data, null, 2));
+    return await this.getBotById(id);
   }
 
   async deleteBot(id) {
     const data = await readAndParse(this.url);
     const withDelete = data.bots.filter((f) => f.id !== Number(id));
+    const length = data.bots.length;
     data.bots = withDelete;
-    return fs.writeFile(this.url, JSON.stringify(data, null, 2));
+    await fs.writeFile(this.url, JSON.stringify(data, null, 2));
+    const deleted = data.bots.length < length;
+    if (deleted) {
+      return "Exito al eliminar"
+    } else {
+      throw new Error("Algo salió mal");
+    }
+
   }
 }
 
